63703dcf56e2335b647aa7040e3f050518cf6061
Hariz <26047712604771@sit.singaporetech.edu.sg>
2026-09-10 16:08:33 +0800

    New Stock value

--- auditor.py
+++ auditor.py
@@ -3,7 +3,11 @@ failed_entries = 0
 Total_Unit_Processed = 0
 while True:
     inventory = input("Enter the inventory count (or 'quit' to exit): ")
-    
+    # Check quit first
+    if inventory == 'quit':
+        break 
+    inventory = int(inventory)
+
 
 
 
